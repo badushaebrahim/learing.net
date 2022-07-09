33a249a27035d2f5c5c27bf342cf1a138d9f9e42
badushaebrahim <badushaebrahim62@gmail.com>
2022-07-09 19:24:42 +0530

    09-07-2022 noon wuth simple ui changes last wipe

--- New folder/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ New folder/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Blank()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
